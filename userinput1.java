163048f27a9de1b4a6f3e97ffb3066841ba13c38
premagk <premapranesh15@gmail.com>
2025-01-05 00:49:42 +0530

    Updated userinput1.java

--- userinput1.java
+++ userinput1.java
@@ -1,31 +1,14 @@
 package chapter6;
 
-import java.util.Scanner;
 
 public class userinput1 {
 	
 	public static void main(String[] args) {
-		
-		
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter name");
-		String name = sc.nextLine();
-		System.out.println("Enter age");
-		int age = sc.nextInt();
-		sc.nextLine();//it is used whenever I use String after int
-		System.out.println("Enter Address");
-		String address = sc.nextLine();
-		
-		
-		
-		
+		int age = 25;
+		String name = "PREMA";
+
 		System.out.println("My name is "+name);	
-		System.out.println("My age is "+age);	
-		System.out.println("address is "+address);
-		
-		
+		System.out.println("My age is "+age);		
 	}
 
 }
